a495c7c2d8aa2368224d3d968a422bad8044db39
SamoKopecky <sano.kopecky@gmail.com>
2020-10-07 17:45:12 +0200

    beutified printing, added more functionality

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import ssl, socket
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def get_cert_info():
-    hostname = 'stackoverflow.com'
+def get_website_info():
+    hostname = 'vutbr.cz'
+    cipher_suite = get_session_info(hostname)
+    print("Info for " + hostname + ": ")
+    cert_pem = bytes(ssl.get_server_certificate((hostname, 443)), 'utf-8')
+    cert = x509.load_pem_x509_certificate(cert_pem, default_backend())
+    print("Certificate Hash algorith : " + cert.signature_hash_algorithm.name)
+    print(get_cert_auth_algorithm(cert.public_key()))
+    print("Certificate valid until : " + str(cert.not_valid_after.date()))
+    print('subject: ')
+    # TODO: nice printing
+    for attribute in cert.subject:
+        print(attribute.oid, end="")
+        print(' = ' + attribute.value)
+    print('issuer:')
+    for attribute in cert.issuer:
+        print(attribute.oid, end="")
+        print(' = ' + attribute.value)
+    print(cert.signature_algorithm_oid)
+    print("Cipher suite : " + cipher_suite[0])
+    print("TLS/SSL version : " + cipher_suite[1])
+
+
+def get_cert_auth_algorithm(public_key):
+    if isinstance(public_key, rsa.RSAPublicKey):
+        return "RSA key size: " + str(public_key.key_size)
+    # TODO: implement more algorithms
+
+
+def get_session_info(hostname):
+    hostname = hostname
     ctx = ssl.create_default_context()
-
     with ctx.wrap_socket(socket.socket(), server_hostname=hostname) as sslsock:
         sslsock.connect((hostname, 443))
         cert = sslsock.cipher()
@@ -15,15 +44,4 @@ def get_cert_info():
 
 
 if __name__ == '__main__':
-    cert_pem = bytes(ssl.get_server_certificate(('stackoverflow.com', 443)), 'utf-8')
-    cert = x509.load_pem_x509_certificate(cert_pem, default_backend())
-    print(cert.signature_hash_algorithm.name)
-    print(cert.not_valid_after)
-    print(cert.not_valid_before)
-    print(cert.issuer)
-    print(cert.subject)
-    print(cert.signature_algorithm_oid)
-    print(get_cert_info())
-    print("exponent : " + str(cert.public_key().public_numbers().e))
-    print(cert.public_key().public_numbers().curve)
-
+    get_website_info()
