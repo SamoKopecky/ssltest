0ad40de15433b3fff5de12cc036fe8b73201e193
SamoKopecky <sano.kopecky@gmail.com>
2020-10-21 15:34:36 +0200

    revert change to pyopenssl lib

--- src/main_info.py
+++ src/main_info.py
@@ -1,38 +1,40 @@
-import OpenSSL.SSL as openssl
+import ssl
 from src.utils import *
 from socket import socket
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 
 def get_website_info(hostname):
-    session = create_session(hostname)
-    get_certificate_info(session)
-    get_session_info(session)
+    get_certificate_info(hostname)
+    get_session_info(hostname)
 
 
-def get_certificate_info(session: openssl.Connection):
-    cert = session.get_peer_certificate()
-    print("Certificate version: " + str(cert.get_version()))
-    print("Serial Number: " + str(cert.get_serial_number()))
-    print("Signature Algorithm: " + str_utf8(cert.get_signature_algorithm()))
-    print("Asymmetric cryptography key length : " + str(cert.get_pubkey().bits()) + " bits")
-    print("Validity interval: " + str(cert.get_notBefore(), 'utf-8') + " to " + str(cert.get_notAfter()))
+def get_certificate_info(hostname):
+    cert_pem = bytes(ssl.get_server_certificate((hostname, 443)), 'utf-8')
+    cert = x509.load_pem_x509_certificate(cert_pem, default_backend())
+    print("Certificate version: " + str(cert.version.value))
+    print("Serial Number: " + str(cert.serial_number))
+    print("Signature Algorithm: " + str(cert.signature_algorithm_oid._name))
+    print("Asymmetric cryptography key length : " + str(cert.public_key().key_size) + " bits")
+    print("Validity interval: " + str(cert.not_valid_before.date()) + " to " + str(cert.not_valid_after.date()))
     print('subject: ')
-    for attribute in cert.get_subject().get_components():
-        print(str_utf8(attribute[0]) + ' = ' + str_utf8(attribute[1]))
+    for attribute in cert.subject:
+        print(attribute.oid._name + ' = ' + attribute.value)
     print('issuer:')
-    for attribute in cert.get_issuer().get_components():
-        print(str_utf8(attribute[0]) + ' = ' + str_utf8(attribute[1]))
+    for attribute in cert.issuer:
+        print(attribute.oid._name + ' = ' + attribute.value)
 
 
-def get_session_info(session):
-    print("Cipher suite : " + session.get_cipher_name())
-    print("TLS/SSL version : " + session.get_protocol_version_name())
+def get_session_info(hostname):
+    session = create_session(hostname)
+    cipher_suite = session.cipher()
+    print("Cipher suite : " + cipher_suite[0])
+    print("TLS/SSL version : " + cipher_suite[1])
 
 
 def create_session(hostname):
-    sock = socket()
-    context = openssl.Context(openssl.TLSv1_2_METHOD)
-    connection = openssl.Connection(context, sock)
-    connection.connect((hostname, 443))
-    connection.do_handshake()
-    return connection
+    ctx = ssl.create_default_context()
+    ssl_socket = ctx.wrap_socket(socket(), server_hostname=hostname)
+    ssl_socket.connect((hostname, 443))
+    return ssl_socket
--- src/utils.py
+++
@@ -1,2 +0,0 @@
-def str_utf8(utf8_encoded_string):
-    return str(utf8_encoded_string, 'utf-8')
