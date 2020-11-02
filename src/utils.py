f0aaec4d627d5982260f6b048d4e06e5c667398e
SamoKopecky <sano.kopecky@gmail.com>
2020-11-02 10:30:37 +0100

    Fix file path to .json file

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 import json
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def convert_openssh_to_iana(search_term):
-    jfile = open('./resources/iana_openssl_cipher_mapping.json', 'r')
+    jfile = open(ROOT_DIR + '/../resources/iana_openssl_cipher_mapping.json', 'r')
     jdata = json.loads(jfile.read())
     for row in jdata:
         if jdata[row] == search_term:
