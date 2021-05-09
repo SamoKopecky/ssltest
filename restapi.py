b10f94ac93a5a7a5cd84e7884998f3dd830261a1
SamoKopecky <sano.kopecky@gmail.com>
2021-05-09 17:15:23 +0200

    Add external visiblity

--- restapi.py
+++ restapi.py
@@ -23,4 +23,4 @@ parser = reqparse.RequestParser()
 parser.add_argument('args', type=str)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001, host='0.0.0.0')
--- server_app/server.py
+++ server_app/server.py
@@ -47,4 +47,4 @@ def result(args=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
