4f4b256360329c401d401c665c85467ec25b5b67
SamoKopecky <sano.kopecky@gmail.com>
2021-10-12 22:32:26 +0200

    Fix bug when no logging arugment is present

--- SSLTest/SSLTest.py
+++ SSLTest/SSLTest.py
@@ -139,12 +139,12 @@ def logging_option(args):
     logger = logging.getLogger(__package__)
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
-
     if args.debug:
-        ch.setLevel(level=logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
     elif args.logging:
-        ch.setLevel(level=logging.INFO)
-
+        ch.setLevel(logging.INFO)
+    else:
+        logging.disable(sys.maxsize)
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
