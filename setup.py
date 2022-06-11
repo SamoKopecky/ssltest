f85fa842ef7e0402024de22e7850eb3904df1cb2
SamoKopecky <skopecky@redhat.com>
2022-06-11 13:19:40 +0200

    Update some information for publishing
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='ssltest',
-    description='',
+    description='Scan web servers cryptographic parameters and vulnerabilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='0.1.1',
     author='Samuel Kopecky',
     author_email='samo.kopecky@protonmail.com',
-    url='https://www.penterep.com/',
+    project_urls={
+        'Source': 'https://github.com/SamoKopecky/ssltest',
+    },
+    url='https://www.penterep.io/',
     licence='GPLv3',
     packages=setuptools.find_packages(),
     classifiers=[
